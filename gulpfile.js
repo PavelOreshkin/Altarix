d6ce991c2de9648fdd881b4e331f557d891e4306
Pavel Oreshkin <poreskin@axbit.ru>
2018-10-05 00:10:28 +0400

    test task is done

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var sass = require('gulp-sass');
+var browserSync = require("browser-sync");
+var reload = browserSync.reload;
+
+
+//
+//Paths
+//
+var path = {
+    build: {
+        html: './',
+        css: './css/',
+        js: './js/',
+        img: './img/'
+    },
+    src: {
+        scss: [
+            './src/scss/*.scss'
+        ],
+        js: [
+            './src/js/**'
+        ],
+        img: './src/img/**'
+    },
+    watch: {
+        scss: './src/scss/**'
+    }
+};
+
+
+gulp.task('browserSync', function () {
+    browserSync({
+        server: {
+            baseDir: "./"
+        },
+        port: 8080,
+        open: true,
+        notify: true
+    });
+});
+
+
+gulp.task('watcher', function () {
+    gulp.watch(path.watch.scss, ['css_dev']);
+});
+
+
+
+//
+//CSS
+//
+
+
+gulp.task('css_dev', function () {
+    return gulp.src(path.src.scss)
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+gulp.task('css_prod', function () {
+    return gulp.src(path.src.scss)
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(path.build.css))
+        .pipe(reload({
+            stream: true
+        }));
+});
+
+
+
+
+//
+//General Tasks
+//
+
+gulp.task('dev', ['css_dev']);
+
+gulp.task('prod', ['css_prod']);
+
+gulp.task('default', ['css_dev', 'watcher', 'browserSync']);
